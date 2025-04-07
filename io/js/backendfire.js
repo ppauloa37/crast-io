9eed624c0e0e1ee7c9a41f513552e38c73258ad3
Paulfox <ppauloa37@gmail.com>
2025-04-07 14:39:58 -0300

    Create backendfire.js

---
+++ io/js/backendfire.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBY7L9WLqev5OsDHoY3M3OCqe99Qmzz-MA",
+  authDomain: "backend-f04d4.firebaseapp.com",
+  projectId: "backend-f04d4",
+  storageBucket: "backend-f04d4.firebasestorage.app",
+  messagingSenderId: "398284485951",
+  appId: "1:398284485951:web:abab5731c0fda95f1b4f8a",
+  measurementId: "G-3T4M7M0JLV"
+};
+firebase.initializeApp(firebaseConfig);
