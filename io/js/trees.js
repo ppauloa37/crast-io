4be4a13c3235fe4e9936d708946080f31bbb2c48
Paulfox <ppauloa37@gmail.com>
2025-04-07 21:30:24 -0300

    Create trees.js

---
+++ io/js/trees.js
@@ -0,0 +1,8 @@
+var arvoreone = document.getElementById("arvoreimage");
+
+var prymarypress = function() {
+    arvoreone.style.marginTop = "100px";
+arvoreone.style.width = "240px"
+}
+prymarypress();
+
