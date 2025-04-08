fd90ae0d025828384f06a104579eb3c2dbf0aef2
Paulfox <ppauloa37@gmail.com>
2025-04-07 21:47:10 -0300

    Create events.js

---
+++ io/js/events.js
@@ -0,0 +1,22 @@
+var button = document.getElementById("inventario");
+var onbuttonItClick = function() {
+inventarioopen.style.opacity = "1";
+
+}
+var onKeyPressed = function(event) {
+    var inventarioopen = document.getElementById("invetory")
+
+event.preventDefault();
+if (event.key === "e" || event.key === "E") {
+    inventarioopen.style.opacity = "1";
+    
+} if (event.key === "d" || event.key === "a") {
+    inventarioopen.style.opacity = "0";
+
+} if (event.key === "w") {
+inventarioopen.style.opacity = "0";
+}
+
+
+document.addEventListener("keydown",onKeyPressed)
+button.addEventListener("click",onbuttonItClick)
